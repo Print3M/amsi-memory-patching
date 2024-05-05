cf4bd6f55df16529ab11ccd7e88d5de2e5292eda
Print3M <92022497+Print3M@users.noreply.github.com>
2024-05-05 20:10:05 +0200

    fix

--- one-liners.ps1
+++
@@ -1,4 +0,0 @@
-###
-# GET SERVICES WITH UNQUOTED PATHS
-###
-Get-CimInstance win32_service | where PathName -notlike "" | where { $_.PathName.StartsWith('"') -eq $false }| select @{e={$_.PathName.split(" ")[0]};n="FirstPath"},PathName | where { $_.FirstPath.EndsWith(".exe") -eq $false } 
